aa5ebb2d627862bda7e3ad6c304bd7f6b7570b22
Tushar <tushargarg0987@gmail.com>
2023-11-02 10:38:03 +0530

    Creating basic web server

---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// album represents data about a record album.
+type album struct {
+    ID     string  `json:"id"`
+    Title  string  `json:"title"`
+    Artist string  `json:"artist"`
+    Price  float64 `json:"price"`
+}
+
+// albums slice to seed record album data.
+var albums = []album{
+    {ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
+    {ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
+    {ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
+}
+
+func getAlbums(c *gin.Context) {
+    c.IndentedJSON(http.StatusOK, albums)
+}
+
+func postAlbums(c *gin.Context) {
+    var newAlbum album
+
+    // Call BindJSON to bind the received JSON to
+    // newAlbum.
+    if err := c.BindJSON(&newAlbum); err != nil {
+        return
+    }
+
+    // Add the new album to the slice.
+    albums = append(albums, newAlbum)
+    c.IndentedJSON(http.StatusCreated, newAlbum)
+}
+
+func getAlbumById(c *gin.Context){
+    id := c.Param("id")
+
+    for _, album := range albums {
+        if album.ID == id {
+            c.IndentedJSON(http.StatusOK, album)
+            return
+        }
+    }
+    c.IndentedJSON(http.StatusNotFound, gin.H{"error": "Album not found"})
+}
+
+func main() {
+    router := gin.Default()
+    router.GET("/albums", getAlbums)
+    router.POST("/albums", postAlbums)
+    router.GET("/albums/:id", getAlbumById)
+
+    router.Run("localhost:8080")
+}
